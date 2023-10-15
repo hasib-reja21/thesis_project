fdd13c0e887f1f626ab4d2fea583ae9e823218a4
hasib-reja <20103080@iubat.edu>
2023-10-15 16:11:13 +0600

    bleading and templating

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\controllers\HomeController;
  use App\Http\controllers\AboutController; 
  use App\Http\controllers\MenuController; 
- use App\Http\controllers\ContactController;
+ use App\Http\controllers\CategoryController;
 
 
 
 // create the controllers 
 
 Route::get("/",[HomeController::class,'home']);
-Route::get("/about",[AboutController::class,'about']);
-Route::get("/menu",[MenuController::class,'menu']);
-Route::get("/contact",[ContactController::class,'contact']);
+Route::get("/category/list",[CategoryController::class,'list']);
+
+
+
 
